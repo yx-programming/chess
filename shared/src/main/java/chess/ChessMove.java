c09a471ebcae552715be8442e0e32cc82b6a8c27
Fred Hancock <yxprogramming@protonmail.com>
2026-09-04 12:47:04 -0600

    add ChessMove basic info stuff not exactly sure why, but == and .equals are different ig. At least it fixed the function.

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -7,23 +9,41 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessMove {
+    private ChessPosition start, end;
+    private ChessPiece.PieceType promotion;
 
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
-                     ChessPiece.PieceType promotionPiece) {
+            ChessPiece.PieceType promotionPiece) {
+        this.start = startPosition;
+        this.end = endPosition;
+        this.promotion = promotionPiece;
+    }
+
+    // equality/hash code
+    @Override public int hashCode() {
+        return Objects.hash(this.start, this.end, this.promotion);
+    }
+    @Override public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (obj.getClass() != this.getClass()) return false;
+        ChessMove other = (ChessMove)obj;
+        return this.start.equals(other.getStartPosition()) &&
+            this.end.equals(other.getEndPosition()) &&
+            this.promotion.equals(other.getPromotionPiece());
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return this.start;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return this.end;
     }
 
     /**
@@ -33,6 +53,6 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return this.promotion;
     }
 }
